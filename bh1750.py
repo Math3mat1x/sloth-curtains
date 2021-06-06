3805b2883f625a79dca55bc5a59432964981c164
Maxime Corne <maxime.corne@protonmail.com>
2021-06-06 22:32:57 +0200

    Shitty BH1750 module found on the internet...

--- bh1750.py
+++ bh1750.py
@@ -1,15 +1,31 @@
-# BH1750 Documentation: https://www.mouser.com/datasheet/2/348/bh1750fvi-e-186247.pdf
+#
+# Librairie permettant de gerer le capteur de luminosite
+# BH1750
+# Tester sur Wemos D1 mini
+#
+# Auteur : iTechnoFrance
+#
 
 from machine import I2C, Pin
 import time
 
-HIGH_RESOLUTION = 0x11 # 0.5 Lux precision, measure time: 120 ms
-HIGH_RESOLUTION_2 = 0x10 # 1 Lux precision, measure time: 120 ms
-LOW_RESOLUTION = 0x13 # # 4 Lux précision, measure time: 16s
-
-ONE_HIGH = 0x21 # 1 measure then idle
-ONE_HIGH_2 = 0x20
-ONE_LOW = 0x23
+BH1750_ADDRESS_ADD_LOW  = 0x5C
+BH1750_ADDRESS_ADD_HIGH = 0x23
+BH1750_POWER_DOWN = 0x00  # Mise en veille
+BH1750_POWER_ON = 0x01  # Attente pour une commande
+BH1750_RESET = 0x07  # Mise à 0 du registre de données seulement en mode POWER_ON
+# Mesure continue, résolution 1 Lux, temps de mesure 120ms
+MODE_CONTINU_HAUTE_RESOLUTION = 0x10
+# Mesure continue, résolution 0.5 Lux, temps de mesure 120ms
+MODE_2_CONTINU_HAUTE_RESOLUTION = 0x11
+# Mesure continue, résolution 4 Lux, temps de mesure 16ms
+MODE_CONTINU_BASSE_RESOLUTION = 0x13
+# 1 mesure puis passe en veille, résolution 1 Lux, temps de mesure 120ms
+MODE_UNE_MESURE_HAUTE_RESOLUTION = 0x20
+# 1 mesure puis passe en veille, résolution 0.5 Lux, temps de mesure 120ms
+MODE_2_UNE_MESURE_HAUTE_RESOLUTION = 0x21
+# 1 mesure puis passe en veille, résolution 4 Lux, temps de mesure 16ms
+MODE_UNE_MESURE_BASSE_RESOLUTION = 0x23 
 
 class BH1750():
        
@@ -19,50 +35,57 @@ class BH1750():
             self.reset()
         
     def detect(self):
-        i2c_peripherals = self.i2c.scan()
-        for i2c_peripheral in i2c_peripherals:    
-            if i2c_peripheral in [0x5C, 0x23]: # low vs high mode
-                self.address = i2c_peripheral
-                print("BH1750 address " + str(self.address))
-                return True
+        detect_bh1750 = False
+        i2c_peripheriques = self.i2c.scan()
+        for i2c_peripherique in i2c_peripheriques:    
+            if (i2c_peripherique == BH1750_ADDRESS_ADD_LOW):
+                self.adresse = BH1750_ADDRESS_ADD_LOW
+                detect_bh1750 = True
+            if (i2c_peripherique == BH1750_ADDRESS_ADD_HIGH):
+                self.adresse = BH1750_ADDRESS_ADD_HIGH
+                detect_bh1750 = True
         return detect_bh1750
     
     def reset(self):
-        data = bytearray(1)
-        data[0] = 0x01 # Power on
-        self.i2c.writeto(self.address, data)  
-        time.sleep(0.01)
-        data[0] = 0x07 # reset
-        self.i2c.writeto(self.address, data)  
-        time.sleep(0.01)
+        self.data = bytearray(1)
+        self.data[0] = BH1750_POWER_ON
+        self.i2c.writeto(self.adresse, self.data)  
+        time.sleep(0.01)  # delai de 10ms
+        self.data[0] = BH1750_RESET
+        self.i2c.writeto(self.adresse, self.data)  
+        time.sleep(0.01)  # delai de 10ms
     
-    def measure(self, mode=False):
-        if not mode:
-            if self.address == 0x5C:
-                mode = LOW_RESOLUTION
-            else:
-                mode = HIGH_RESOLUTION
-
-        data_mode = bytearray(1)
-        lux = bytearray(2)
-        delay = 0
-
-        if mode in (ONE_HIGH, ONE_HIGH_2):
-            delay = 0.120
-        if mode == ONE_LOW:
-            delay = 0.016
-
-        data_mode[0] = mode
-        self.i2c.writeto(self.address, data_mode)
-        time.sleep(delay)
-
-        try:
-            self.i2c.readfrom(self.address, lux[0])
-        except TypeError:
-            print(self.address)
-            print(lux)
-
-        lux = lux[0] * 256 + lux[1]
-        lux = float(lux) / 1.2
-
-        return lux
+    def lecture_lumiere(self, mode_mesure): 
+        self.BH1750_MODE = mode_mesure
+        self.mode_mesure_ok = False
+        self.luminosite = 0
+        self.data_mode = bytearray(1)
+        self.lux = bytearray(2)
+        self.delai = 0
+        # configuration du mode de mesure
+        if (self.BH1750_MODE == MODE_CONTINU_HAUTE_RESOLUTION):
+            self.mode_mesure_ok = True
+        if (self.BH1750_MODE == MODE_2_CONTINU_HAUTE_RESOLUTION):
+            self.mode_mesure_ok = True
+        if (self.BH1750_MODE == MODE_CONTINU_BASSE_RESOLUTION):
+            self.mode_mesure_ok = True
+        if (self.BH1750_MODE == MODE_UNE_MESURE_HAUTE_RESOLUTION):
+            self.mode_mesure_ok = True
+            self.delai = 0.120
+        if (self.BH1750_MODE == MODE_2_UNE_MESURE_HAUTE_RESOLUTION):
+            self.mode_mesure_ok = True
+            self.delai = 0.120
+        if (self.BH1750_MODE == MODE_UNE_MESURE_BASSE_RESOLUTION):
+            self.mode_mesure_ok = True
+            self.delai = 0.016
+        if (self.mode_mesure_ok):
+            self.data_mode[0] = self.BH1750_MODE
+            self.i2c.writeto(self.adresse, self.data_mode)
+            time.sleep(self.delai)
+            self.i2c.readfrom_into(self.adresse, self.lux)
+            self.lux = self.lux[0] * 256 + self.lux[1]
+            self.lux = int(float(self.lux) / 1.2)  # Voir la documentation du capteur
+            return(self.lux)
+        else:
+            print("Erreur : mode invalide")
+        
