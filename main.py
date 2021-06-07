d6cfb579841f7cb0ed7a8614b9836250c53af0f9
Maxime Corne <maxime.corne@protonmail.com>
2021-06-07 18:05:31 +0200

    Fix and tweak the default script.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import bh1750  # librairie capteur BH1750
 # Declaration I2C
 # // D1 -> GPIO05 –> SCL
 # // D2 -> GPIO04 –> SDA
-i2c = machine.I2C(scl = machine.Pin(5), sda = machine.Pin(4), freq=400000)
+i2c = machine.I2C(id=0, scl = machine.Pin(17), sda = machine.Pin(16), freq=400000)
 capteur_lumiere = bh1750.BH1750(i2c)  # declaration BH1750
 if (capteur_lumiere.detect()):
     # Resolutions et modes possibles pour les mesures
@@ -19,9 +19,10 @@ if (capteur_lumiere.detect()):
     # MODE_UNE_MESURE_HAUTE_RESOLUTION = 1 mesure puis passe en veille, résolution 1 Lux, temps de mesure 120ms
     # MODE_2_UNE_MESURE_HAUTE_RESOLUTION = 1 mesure puis passe en veille, résolution 0.5 Lux, temps de mesure 120ms
     # MODE_UNE_MESURE_BASSE_RESOLUTION = 1 mesure puis passe en veille, résolution 4 Lux, temps de mesure 16ms
-    while True:
-        mesure_lux = capteur_lumiere.lecture_lumiere(bh1750.MODE_CONTINU_HAUTE_RESOLUTION)
-        print(mesure_lux)
-        time.sleep(1)
+    def test_sensor():
+        while True:
+            mesure_lux = capteur_lumiere.lecture_lumiere(bh1750.MODE_CONTINU_HAUTE_RESOLUTION)
+            print(mesure_lux)
+            time.sleep(1)
 else:
-   print(« Capteur BH1750 non detecte »)
+   print("Capteur BH1750 non detecte")
